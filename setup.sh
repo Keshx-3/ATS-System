4f2599ebc212bad6945341e2fd32ad9a583937ba
Kesavadas <kesavadaskesh@gmail.com>
2025-03-23 14:41:20 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-#!/bin/bash 
-echo "Running setup.sh" 
-python -m spacy download en_core_web_sm 
+#!/bin/bash
+echo "Starting setup.sh" > /tmp/setup_log.txt
+python -m spacy download en_core_web_sm 2>> /tmp/setup_log.txt
+if [ $? -eq 0 ]; then
+    echo "Successfully installed en_core_web_sm" >> /tmp/setup_log.txt
+else
+    echo "Failed to install en_core_web_sm" >> /tmp/setup_log.txt
+fi
+echo "Finished setup.sh" >> /tmp/setup_log.txt
